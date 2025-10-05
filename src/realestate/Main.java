fd198006a80b57a1b27ed8f8d6dd953ccac61b29
Lattuse <sultik280805@gmail.com>
2025-10-05 21:43:26 +0500

    main

--- src/realestate/Main.java
+++ src/realestate/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package realestate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Property house = new House(
+                "123 Groove Street",
+                250000, 1500,
+                "Biba",
+                new Sale(),
+                new Rent()
+        );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Property apartment = new Apartment(
+                "45 Smirnovo Avenue, Apt 12B",
+                100000, 800,
+                "Boba",
+                new Rent(),
+                new Sale()
+        );
+
+        house.manage();
+        System.out.println();
+        apartment.manage();
     }
 }
+
